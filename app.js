8d1a5f7aaaad60b74d905e955c7889e01bee12c8
heartfulnessgujarat <heartfulness.gujarat@gmail.com>
2026-03-19 19:41:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const params=new URLSearchParams(window.location.search);
 
 const EVENT_ID=params.get("event");
 
-let RULES=[];
-let OPTIONS=[];
-let EVENT={};
+let PARTICIPANT=null;
 
 let EDIT_MODE=false;
 
@@ -22,147 +20,43 @@ await fetch(API_URL+"?event="+EVENT_ID);
 let data=
 await res.json();
 
-RULES=data.rules;
-
-OPTIONS=data.options;
-
-EVENT=data.event;
-
-buildTitles(data.titles);
-
-buildForm();
+buildNameField();
 
 hideLoading();
 
-initButtons();
-
 }
 
-function initButtons(){
-
-document.getElementById("editBtn").onclick=enableEdit;
-
-document.getElementById("registerBtn").onclick=registerInitial;
-
-document.getElementById("updateBtn").onclick=registerUpdated;
-
-}
-
-function enableEdit(){
-
-EDIT_MODE=true;
-
-RULES.forEach(field=>{
-
-let el=document.getElementById(field.Field_Name);
-
-if(!el) return;
-
-if(field.Editable=="Yes"){
-
-el.disabled=false;
-
-}
-
-});
-
-document.getElementById("registerBtn").style.display="none";
-
-document.getElementById("editBtn").style.display="none";
-
-document.getElementById("updateBtn").style.display="inline";
-
-validateForm();
-
-}
-
-function registerInitial(){
-
-alert("Ready for submission");
-
-}
-
-function registerUpdated(){
-
-alert("Updated submission ready");
-
-}
-
-function showLoading(){
-
-document.getElementById("loading").style.display="block";
-
-document.getElementById("buttonArea").style.display="none";
-
-}
-
-function hideLoading(){
-
-document.getElementById("loading").style.display="none";
-
-document.getElementById("buttonArea").style.display="block";
-
-}
-
-function buildTitles(titles){
-
-let title=titles.find(t=>t.Property=="Event Title");
-
-let subtitle=titles.find(t=>t.Property=="Event Subtitle");
-
-document.getElementById("eventTitle").innerText=title?title.Value:"";
-
-document.getElementById("eventSubtitle").innerText=subtitle?subtitle.Value:"";
-
-}
-
-function buildForm(){
+function buildNameField(){
 
 let form=document.getElementById("dynamicForm");
 
 form.innerHTML="";
 
-RULES
-.sort((a,b)=>a.Field_Order-b.Field_Order)
-.forEach(field=>{
-
-if(field.Show!="Yes") return;
-
 let label=document.createElement("label");
 
-label.innerText=field.Field_Label;
+label.innerText="Name";
 
 form.appendChild(label);
 
 let input=document.createElement("input");
 
+input.id="Name";
+
 input.type="text";
-
-input.id=field.Field_Name;
-
-if(field.Editable!="Yes"){
-
-input.disabled=true;
-
-}
 
 input.oninput=function(){
 
-validateForm();
-
-if(field.Field_Name=="Name"){
-
 searchName(this.value);
-
-}
 
 };
 
 form.appendChild(input);
 
-form.appendChild(document.createElement("br"));
+let list=document.createElement("div");
 
-});
+list.id="nameList";
+
+form.appendChild(list);
 
 }
 
@@ -185,17 +79,9 @@ showNameList(data.participants);
 
 function showNameList(list){
 
-let existing=document.getElementById("nameList");
+let div=document.getElementById("nameList");
 
-if(existing){
-
-existing.remove();
-
-}
-
-let div=document.createElement("div");
-
-div.id="nameList";
+div.innerHTML="";
 
 list.forEach(p=>{
 
@@ -215,49 +101,147 @@ div.appendChild(item);
 
 });
 
-document.getElementById("Name")
-.after(div);
-
 }
 
 function selectParticipant(p){
 
-RULES.forEach(field=>{
+PARTICIPANT=p;
 
-let el=document.getElementById(field.Field_Name);
+document.getElementById("nameList").innerHTML="";
 
-if(el && p[field.Field_Name]){
-
-el.value=p[field.Field_Name];
+buildDetails();
 
 }
 
-});
+function buildDetails(){
 
-document.getElementById("nameList").remove();
+let form=document.getElementById("dynamicForm");
+
+form.innerHTML="";
+
+addField("Name",PARTICIPANT.Name,false);
+
+addField("Mobile",PARTICIPANT.Mobile,false);
+
+addField("Email",PARTICIPANT.Email,false);
+
+addField("Centre",PARTICIPANT.Centre,false);
+
+addField("District",PARTICIPANT.District,false);
+
+addField("Zone",PARTICIPANT.Zone,false);
+
+addField("SRCMID",PARTICIPANT.SRCMID,false);
+
+addField("PINCODE",PARTICIPANT.PINCODE,false);
+
+showButtons();
+
+}
+
+function addField(name,value,editable){
+
+let form=document.getElementById("dynamicForm");
+
+let label=document.createElement("label");
+
+label.innerText=name;
+
+form.appendChild(label);
+
+let input=document.createElement("input");
+
+input.id=name;
+
+input.value=value;
+
+input.disabled=!editable;
+
+form.appendChild(input);
+
+form.appendChild(document.createElement("br"));
 
 }
 
-function validateForm(){
+function showButtons(){
 
-let valid=true;
+document.getElementById("buttonArea")
+.style.display="block";
 
-RULES.forEach(field=>{
+document.getElementById("registerBtn")
+.style.display="inline";
 
-if(field.Mandatory!="Yes") return;
-
-let el=document.getElementById(field.Field_Name);
-
-if(!el) return;
-
-if(el.value=="") valid=false;
-
-});
-
-if(EDIT_MODE){
-
-document.getElementById("updateBtn").disabled=!valid;
+document.getElementById("editBtn")
+.style.display="inline";
 
 }
+
+document.getElementById("editBtn").onclick=function(){
+
+enableEdit();
+
+};
+
+function enableEdit(){
+
+EDIT_MODE=true;
+
+document.getElementById("registerBtn")
+.disabled=true;
+
+document.getElementById("editBtn")
+.style.display="none";
+
+document.getElementById("updateBtn")
+.style.display="inline";
+
+enableField("Mobile");
+
+enableField("Email");
+
+enableField("Centre");
+
+enableField("PINCODE");
+
+}
+
+function enableField(name){
+
+let el=document.getElementById(name);
+
+if(el){
+
+el.disabled=false;
+
+}
+
+}
+
+document.getElementById("registerBtn").onclick=function(){
+
+alert("Registration ready");
+
+};
+
+document.getElementById("updateBtn").onclick=function(){
+
+alert("Updated registration ready");
+
+};
+
+function showLoading(){
+
+document.getElementById("loading")
+.style.display="block";
+
+document.getElementById("buttonArea")
+.style.display="none";
+
+}
+
+function hideLoading(){
+
+document.getElementById("loading")
+.style.display="none";
 
 }
