cbdef965e67c99f035f8049abe5bad618831a22e
heartfulnessgujarat <heartfulness.gujarat@gmail.com>
2026-03-21 23:29:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+alert("NEW VERSION LOADED");
+
 const API="https://script.google.com/macros/s/AKfycbwMXVVLzngiMoQr3XDlOKldn-_n0qflFgVxInhvkVQD5K5EKOeStm9v0q3hrSlJDjwT/exec";
 
 const params=new URLSearchParams(window.location.search);
